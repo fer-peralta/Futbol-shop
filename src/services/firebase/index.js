baf98134d6da194b8b63a0bb99067df4f2aa0205
Fernando Peralta <fernandoemanuelperalta@gmail.com>
2022-09-07 19:26:26 -0300

    env

--- src/services/firebase/index.js
+++ src/services/firebase/index.js
@@ -4,12 +4,12 @@ import {getFirestore} from "firebase/firestore"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBbcIHCX1hGHMATb5t-tZqTprLiaLxp2EQ",
-  authDomain: "futbol-shop-7aff8.firebaseapp.com",
-  projectId: "futbol-shop-7aff8",
-  storageBucket: "futbol-shop-7aff8.appspot.com",
-  messagingSenderId: "307500929073",
-  appId: "1:307500929073:web:67ae59bed90f20db1e4510"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
